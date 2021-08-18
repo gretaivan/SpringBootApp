aabe89e9c072a09aee6669a0fe9c05233a08c4e2
gretaivan <greta.ivan91@gmail.com>
2021-08-18 12:06:58 +0100

    adds post request mapping

--- src/main/java/codes/greta/SpringBootApp/controllers/CourseEndpoint.java
+++ src/main/java/codes/greta/SpringBootApp/controllers/CourseEndpoint.java
@@ -1,16 +1,23 @@
 package codes.greta.SpringBootApp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import codes.greta.SpringBootApp.models.Course;
+
 @RestController
 public class CourseEndpoint {
 
+    @CrossOrigin(origins = "*")
     @RequestMapping("/course")
     public Course getEndpoint(@RequestParam(value="name", defaultValue = "Spring Boot", required = false)String name,
                               @RequestParam(value="chapterCount", defaultValue = "2", required = false)int chapterCount){
         return new Course(name, chapterCount);
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(method = RequestMethod.POST, value = "/register/course")
+    public String saveCourse(@RequestBody Course course) {
+        return "Your course named " + course.getName()  + " with " + course.getChapterCount() +  " has been saved successfully!";
     }
 
 }
