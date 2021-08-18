5b7bb50f020a53b9d46a9a6c2dc94f1fb1fd4ffa
gretaivan <greta.ivan91@gmail.com>
2021-08-18 12:15:42 +0100

    adds the default value injection and example of override for application.properties

--- src/main/java/codes/greta/SpringBootApp/controllers/CourseEndpoint.java
+++ src/main/java/codes/greta/SpringBootApp/controllers/CourseEndpoint.java
@@ -1,11 +1,24 @@
 package codes.greta.SpringBootApp.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import codes.greta.SpringBootApp.models.Course;
 
 @RestController
 public class CourseEndpoint {
+
+    //default application property injection
+    @Value("${default.course.name}")
+    private String cName;
+
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping("/default")
+    public Course getDefault(@RequestParam(value="name", required = false)String name,
+                              @RequestParam(value="chapterCount", defaultValue = "2", required = false)int chapterCount){
+        return new Course(cName, chapterCount);
+    }
 
     @CrossOrigin(origins = "*")
     @RequestMapping("/course")
